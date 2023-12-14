d6dc4dfd1c9d5bcca3ab63dba5d2fe141a0e817b
Per(k) <aupper094@gmail.com>
2023-12-14 23:03:28 +0700

    Start basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from './home/home.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, NavbarComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, HomeComponent],
+
 })
 export class AppComponent {
   title = 'my-app';
